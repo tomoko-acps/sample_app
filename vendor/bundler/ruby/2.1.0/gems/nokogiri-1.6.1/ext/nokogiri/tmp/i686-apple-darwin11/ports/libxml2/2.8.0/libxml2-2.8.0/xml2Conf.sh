5c6dc7c4057987c4a46d9e56fbaff164625d5df0
tomoko-acps <katsube@tomoko-no-MacBook-Pro.local>
2014-05-07 14:14:47 +0900

    initial commit

---
+++ vendor/bundler/ruby/2.1.0/gems/nokogiri-1.6.1/ext/nokogiri/tmp/i686-apple-darwin11/ports/libxml2/2.8.0/libxml2-2.8.0/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/Volumes/SSPHDU3/project/99_sample/RailsApp/sample_app/vendor/bundler/ruby/2.1.0/gems/nokogiri-1.6.1/ports/i686-apple-darwin11/libxml2/2.8.0/lib"
+XML2_LIBS="-lxml2 -lz -lpthread  -liconv -lm "
+XML2_INCLUDEDIR="-I/Volumes/SSPHDU3/project/99_sample/RailsApp/sample_app/vendor/bundler/ruby/2.1.0/gems/nokogiri-1.6.1/ports/i686-apple-darwin11/libxml2/2.8.0/include/libxml2"
+MODULE_VERSION="xml2-2.8.0"
+
---
+++ vendor/bundler/ruby/2.1.0/gems/nokogiri-1.6.1/ext/nokogiri/tmp/i686-apple-darwin11/ports/libxslt/1.1.26/libxslt-1.1.26/xsltConf.sh
@@ -0,0 +1,7 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/Volumes/SSPHDU3/project/99_sample/RailsApp/sample_app/vendor/bundler/ruby/2.1.0/gems/nokogiri-1.6.1/ports/i686-apple-darwin11/libxslt/1.1.26/lib"
+XSLT_LIBS="-lxslt  -L/Volumes/SSPHDU3/project/99_sample/RailsApp/sample_app/vendor/bundler/ruby/2.1.0/gems/nokogiri-1.6.1/ports/i686-apple-darwin11/libxml2/2.8.0/lib -lxml2 -lz -lpthread -liconv -lm "
+XSLT_INCLUDEDIR="-I/Volumes/SSPHDU3/project/99_sample/RailsApp/sample_app/vendor/bundler/ruby/2.1.0/gems/nokogiri-1.6.1/ports/i686-apple-darwin11/libxslt/1.1.26/include"
+MODULE_VERSION="xslt-1.1.26"
