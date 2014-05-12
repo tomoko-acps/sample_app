ac0007097b6574b11f95b82e051142479e15b5e2
tomoko-acps <katsube@tomoko-no-MacBook-Pro.local>
2014-05-12 16:02:28 +0900

    MAke a basic User Model (including secure passwords)

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.1.1'
 gem 'rails', '4.1.1'
 gem 'bootstrap-sass', '2.3.2.0'
 gem 'sprockets', '2.11.0'
+gem 'bcrypt-ruby', '3.1.2'
 # Use sqlite3 as the database for Active Record
 group :development, :test do
   gem 'sqlite3', '1.3.8'
@@ -18,6 +19,7 @@ end
 group :test do
   gem 'selenium-webdriver', '2.35.1'
   gem 'capybara', '2.1.0'
+  gem 'minitest'
 end
 # Use SCSS for stylesheets
 gem 'sass-rails', '4.0.2'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,29 +4,56 @@ require 'support/utilities'
 describe "Static Page" do
 
 	subject { page }
+
+	shared_examples_for "all static pages" do
+		it { expect have_content(heading) }
+		it { expect have_title(full_title(page_title)) }
+	end
 	describe "Home page" do
 		before {visit root_path}
-		it { expect have_content('Sample App')}
-		it { expect have_title(full_title(''))}
+		let(:heading)   { 'Sample App' }
+		let(:page_title){ '' }
+
+		it_should_behave_like "all static pages"
 		it { expect be_falsy have_title('| Home')} # 否定形の時
 	end
 
 	describe "Help page" do
 		before {visit help_path}
-		it {expect have_content('Help')}
-		it {expect have_title(full_title('Help'))}
+		let(:heading)   { 'Help' }
+		let(:page_title){ heading }
+
+		it_should_behave_like "all static pages"
 	end
 
 	describe "About page" do
 		before {visit about_path}
-		it {expect have_content('About Us');}
-		it {expect have_title(full_title('About Us'))}
+		let(:heading)   { 'About Us' }
+		let(:page_title){ heading }
+
+		it_should_behave_like "all static pages"
 	end
 
 	describe "Contact page" do
 		before {visit contact_path}
-		it {expect have_content('Contact')}
-		it {expect have_title(full_title('Contact'))}
+		let(:heading)   { 'Contact' }
+		let(:page_title){ heading }
+
+		it_should_behave_like "all static pages"
+	end
+
+	# レイアウトのリンクをテストする
+	it "expect have the right links on the layout" do
+		visit root_path
+		click_link "About"
+		expect have_title(full_title('About Us'))
+		click_link "Help"
+		expect have_title(full_title('Help'))
+		click_link "Home"
+		click_link "Sign up now!"
+		expect have_title(full_title('Sign up'))
+		click_link "sample app"
+		expect have_title(full_title(''))
 	end
 
 end
